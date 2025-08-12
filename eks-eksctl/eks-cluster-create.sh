a035e80d4ce77c15a391089624411965fea8d0b4
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2025-08-12 22:13:21 +0300

    Add new Bottlerocket cluster configuration and update existing EKS setup
    - Introduced a new configuration file `cks-prep-cluster-bottlerocket.yaml` for a Bottlerocket-based EKS cluster.
    - Updated existing `cks-prep-cluster.yaml` to change the managed node group name and AMI family to Amazon Linux 2023.
    - Enhanced `eks-cluster-create.sh` script to include cluster update commands and instance querying for running instances.

--- eks-eksctl/eks-cluster-create.sh
+++ eks-eksctl/eks-cluster-create.sh
@@ -1,8 +1,18 @@
 eksctl create cluster -f cks-prep-cluster.yaml
 
+# eksctl upgrade cluster -f cks-prep-cluster.yaml
+
 eksctl utils write-kubeconfig --cluster=cks-prep-cluster --region=us-east-1
 
 
 # https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html
 # https://docs.aws.amazon.com/eks/latest/userguide/lbc-helm.html
 
+# aws ssm start-session --target <instance-id>
+
+aws ec2 describe-instances \
+  --region us-east-1 \
+  --filters "Name=tag:eks:cluster-name,Values=cks-prep-cluster" \
+            "Name=instance-state-name,Values=running" \
+  --query "Reservations[].Instances[].InstanceId" \
+  --output text
